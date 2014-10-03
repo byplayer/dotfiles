b0ee154bfdd324dc2171a5dbc3a604d705fdce40
Yukio Goto <yukio.goto@mail.rakuten.com>
2014-10-03 09:14:30 +0800

    upgrade craftLaunch version configuration

--- clnch/config.py
+++ clnch/config.py
@@ -9,35 +9,35 @@ def configure(window):
     # --------------------------------------------------------------------
     # F1 キーでヘルプファイルを起動する
     def command_Help():
-        print u"Helpを起動 :"
+        print("Helpを起動 :")
         help_path = os.path.join( os.path.split(sys.argv[0])[0], 'doc\\index.html' )
-        shellExecute( None, None, help_path, u"", u"" )
-        print u"Done.\n"
+        shellExecute( None, None, help_path, "", "" )
+        print("Done.\n")
 
-    window.cmd_keymap[ VK_F1 ] = command_Help
+    window.cmd_keymap[ "F1" ] = command_Help
 
     # --------------------------------------------------------------------
     # Ctrl-E で、入力中のファイルパスを編集する
-    window.cmd_keymap[ KeyEvent( VK_E, MODKEY_CTRL ) ] = window.command_Edit
+    window.cmd_keymap[ "C-E" ] = window.command.Edit
 
     # --------------------------------------------------------------------
     # テキストエディタを設定する
-    window.editor = u"WZEDITOR.exe"
+    window.editor = "C:/goto/tools/SublimeText2/sublime_text.exe"
 
     # --------------------------------------------------------------------
     # ファイルタイプごとの動作設定
     window.association_list += [
-        ( "*.mpg *.mpeg *.avi *.wmv", window.command_ShellExecute( None, u"wmplayer.exe", "/prefetch:7 /Play %param%", u"" ) ),
+        ( "*.mpg *.mpeg *.avi *.wmv", window.ShellExecuteCommand( None, "wmplayer.exe", "/prefetch:7 /Play %param%", "" ) ),
     ]
 
     # --------------------------------------------------------------------
     # 非アクティブ時の時計の形式
     if 1:
         # 年と秒を省略した短い形式
-        window.clock_format = u"%m/%d(%a) %H:%M"
+        window.clock_format = "%m/%d(%a) %H:%M"
     else:
         # 年月日(曜日) 時分秒 の全てを表示する
-        window.clock_format = u"%Y/%m/%d(%a) %H:%M:%S"
+        window.clock_format = "%Y/%m/%d(%a) %H:%M:%S"
 
     # --------------------------------------------------------------------
     # 空欄コマンド
@@ -60,7 +60,7 @@ def configure(window):
         elif mod==MODKEY_SHIFT|MODKEY_CTRL:
             pyauto.Window.enum( callback, ["mintty.exe","MinTTY"] )
 
-    window.launcher.command_list += [ ( u"", command_QuickActivate ) ]
+    window.launcher.command_list += [ ( "", command_QuickActivate ) ]
 
     # --------------------------------------------------------------------
     # "NetDrive" コマンド
@@ -75,37 +75,41 @@ def configure(window):
                 path = args[1]
                 checkNetConnection(path)
                 if window.subProcessCall( [ "net", "use", drive_letter+":", os.path.normpath(path), "/yes" ], cwd=None, env=None, enable_cancel=False )==0:
-                    print u"%s に %sドライブを割り当てました。" % ( path, drive_letter )
+                    print("%s に %sドライブを割り当てました。" % ( path, drive_letter ))
             else:
                 if window.subProcessCall( [ "net", "use", drive_letter+":", "/D" ], cwd=None, env=None, enable_cancel=False )==0:
-                    print u"%sドライブの割り当てを解除しました。" % ( drive_letter )
+                    print("%sドライブの割り当てを解除しました。" % ( drive_letter ))
         else:
-            print u"ドライブの割り当て : NetDrive;<ドライブ名>;<パス>"
-            print u"ドライブの解除     : NetDrive;<ドライブ名>"
+            print("ドライブの割り当て : NetDrive;<ドライブ名>;<パス>")
+            print("ドライブの解除     : NetDrive;<ドライブ名>")
 
     # --------------------------------------------------------------------
     # コマンドを登録する
     window.launcher.command_list += [
-        ( u"alc",       window.command_ShellExecute( None, u"C:/Users/yukio.goto/AppData/Local/Google/Chrome/Application/chrome.exe", u"http://eow.alc.co.jp/%param%/UTF-8/", u"C:/Users/yukio.goto/AppData/Local/Google/Chrome/Application/" ) ),
-        ( u"Becky",     window.command_ShellExecute( None, u"C:/Program Files (x86)/OnlineServiceGate/OnlineServiceGate.Client.MailerStarterForBecky.exe", u"", u"C:/Program Files (x86)/OnlineServiceGate/" ) ),
-        ( u"chrome",    window.command_ShellExecute( None, u"C:/Users/yukio.goto/AppData/Local/Google/Chrome/Application/chrome.exe", u"", u"C:/Users/yukio.goto/AppData/Local/Google/Chrome/Application/" ) ),
-        ( u"ie32",        window.command_ShellExecute( None, u"C:/Program Files (x86)/Internet Explorer/iexplore.exe", u"", u"C:/Program Files (x86)/Internet Explorer/" ) ),
-        ( u"ie64",        window.command_ShellExecute( None, u"C:/Program Files/Internet Explorer/iexplore.exe", u"", u"C:/Program Files/Internet Explorer/" ) ),
-        ( u"df",        window.command_ShellExecute( None, u"C:/goto/tools/DF/DF.exe", u"", u"C:/goto/tools/DF" ) ),
-        ( u"eyes",      window.command_ShellExecute( None, u"C:/Program Files (x86)/MA-EYES/MA-EYES.exe", u"", u"C:/Program Files (x86)/MA-EYES" ) ),
-        ( u"vbox",      window.command_ShellExecute( None, u"C:/goto/tools/Oracle/VirtualBox/VirtualBox.exe", u"", u"C:/goto/tools/Oracle/VirtualBox" ) ),
-        ( u"Poderosa",  window.command_ShellExecute( None, u"C:/goto/tools/Poderosa/Poderosa.exe", u"", u"C:/goto/tools/Poderosa" ) ),
-        ( u"GanttProject",  window.command_ShellExecute( None, u"C:/goto/tools/GanttProject/ganttproject.exe", u"", u"C:/goto/tools/GanttProject" ) ),
-        ( u"wz",        window.command_ShellExecute( None, u"C:/goto/tools/WZ Editor/WZEDITOR.EXE", u"", u"C:/goto/tools/WZ Editor" ) ),
-        ( u"kf",        window.command_ShellExecute( None, u"C:/goto/tools/KF/KF.EXE", u"", u"C:/goto/tools/KF" ) ),
-        ( u"WinDirStat",window.command_ShellExecute( None, u"C:/goto/tools/WinDirStat/windirstat.exe", u"", u"C:/goto/tools/WinDirStat" ) ),
-        ( u"projects",  window.command_ShellExecute( None, u"s:/projects", u"", u"" ) ),
-        ( u"ProcMon",window.command_ShellExecute( None, u"C:/goto/tools/Sysinternals/Procmon.exe", u"", u"C:/goto/tools/Sysinternals" ) ),
-        ( u"ProcExp",window.command_ShellExecute( None, u"C:/goto/tools/Sysinternals/Procexp.exe", u"", u"C:/goto/tools/Sysinternals" ) ),
-        ( u"proxy",window.command_ShellExecute( None, u"C:/squid/bin/squid_env.bat", u"%param%", u"C:/squid/bin/" ) ),
-        ( u"Meadow",window.command_ShellExecute( None, u"C:/goto/tools/meadow/bin/RunMW32.exe", u"", u"C:/Goto/tools/meadow/bin" ) ),
+        ( "alc",       window.ShellExecuteCommand( None, "C:/Users/yukio.goto/AppData/Local/Google/Chrome/Application/chrome.exe", "http://eow.alc.co.jp/%param%/UTF-8/", "C:/Users/yukio.goto/AppData/Local/Google/Chrome/Application/" ) ),
+        ( "Becky",     window.ShellExecuteCommand( None, "C:/Program Files (x86)/OnlineServiceGate/OnlineServiceGate.Client.MailerStarterForBecky.exe", "", "C:/Program Files (x86)/OnlineServiceGate/" ) ),
+        ( "chrome",    window.ShellExecuteCommand( None, "C:/Users/yukio.goto/AppData/Local/Google/Chrome/Application/chrome.exe", "", "C:/Users/yukio.goto/AppData/Local/Google/Chrome/Application/" ) ),
+        ( "ie32",        window.ShellExecuteCommand( None, "C:/Program Files (x86)/Internet Explorer/iexplore.exe", "", "C:/Program Files (x86)/Internet Explorer/" ) ),
+        ( "ie64",        window.ShellExecuteCommand( None, "C:/Program Files/Internet Explorer/iexplore.exe", "", "C:/Program Files/Internet Explorer/" ) ),
+        ( "df",        window.ShellExecuteCommand( None, "C:/goto/tools/DF/DF.exe", "", "C:/goto/tools/DF" ) ),
+        ( "eyes",      window.ShellExecuteCommand( None, "C:/Program Files (x86)/MA-EYES/MA-EYES.exe", "", "C:/Program Files (x86)/MA-EYES" ) ),
+        ( "vbox",      window.ShellExecuteCommand( None, "C:/goto/tools/Oracle/VirtualBox/VirtualBox.exe", "", "C:/goto/tools/Oracle/VirtualBox" ) ),
+        ( "Poderosa",  window.ShellExecuteCommand( None, "C:/goto/tools/Poderosa/Poderosa.exe", "", "C:/goto/tools/Poderosa" ) ),
+        ( "GanttProject",  window.ShellExecuteCommand( None, "C:/goto/tools/GanttProject/ganttproject.exe", "", "C:/goto/tools/GanttProject" ) ),
+        ( "wz",        window.ShellExecuteCommand( None, "C:/goto/tools/WZ Editor/WZEDITOR.EXE", "", "C:/goto/tools/WZ Editor" ) ),
+        ( "kf",        window.ShellExecuteCommand( None, "C:/goto/tools/KF/KF.EXE", "", "C:/goto/tools/KF" ) ),
+        ( "WinDirStat",window.ShellExecuteCommand( None, "C:/goto/tools/WinDirStat/windirstat.exe", "", "C:/goto/tools/WinDirStat" ) ),
+        ( "projects",  window.ShellExecuteCommand( None, "s:/projects", "", "" ) ),
+        ( "ProcMon",window.ShellExecuteCommand( None, "C:/goto/tools/Sysinternals/Procmon.exe", "", "C:/goto/tools/Sysinternals" ) ),
+        ( "ProcExp",window.ShellExecuteCommand( None, "C:/goto/tools/Sysinternals/Procexp.exe", "", "C:/goto/tools/Sysinternals" ) ),
+        ( "proxy",window.ShellExecuteCommand( None, "C:/squid/bin/squid_env.bat", "%param%", "C:/squid/bin/" ) ),
+        ( "Meadow",window.ShellExecuteCommand( None, "C:/goto/tools/meadow/bin/RunMW32.exe", "", "C:/Goto/tools/meadow/bin" ) ),
+        ( "tail",window.ShellExecuteCommand( None, "C:/Goto/tools/tail/tail.exe", "%param%", "C:/Goto/tools/tail/" ) ),
+        ( "eclipse",window.ShellExecuteCommand( None, "C:/goto/tools/eclipse/eclipse.exe", "", "C:/goto/tools/eclipse/" ) ),
+        ( "SublimeText",window.ShellExecuteCommand( None, "C:/goto/tools/SublimeText2/sublime_text.exe", "", "C:/goto/tools/SublimeText2/" ) ),
+        ( "ADT",window.ShellExecuteCommand( None, "C:/goto/tools/adt-bundle-windows-x86_64/eclipse/eclipse.exe", "", "C:/goto/tools/adt-bundle-windows-x86_64/eclipse" ) ),
+        ( "vagrant up",window.ShellExecuteCommand( None, "vagrant", "up", "C:/Goto/vagrant-devenv" ) ),
     ]
-
 
 # リストウインドウの設定処理
 def configure_ListWindow(window):
@@ -113,11 +117,10 @@ def configure_ListWindow(window):
     # --------------------------------------------------------------------
     # F1 キーでヘルプファイルを表示する
 
-    def command_Help():
-        print u"Helpを起動 :"
-        help_path = os.path.join( os.path.split(sys.argv[0])[0], 'doc\\index.html' )
-        shellExecute( None, None, help_path, u"", u"" )
-        print u"Done.\n"
+    def command_Help(info):
+        print( "Helpを起動 :" )
+        help_path = os.path.join( getAppExePath(), 'doc\\index.html' )
+        shellExecute( None, help_path, "", "" )
+        print( "Done.\n" )
 
-    window.keymap[ VK_F1 ] = command_Help
-
+    window.keymap[ "F1" ] = command_Help
